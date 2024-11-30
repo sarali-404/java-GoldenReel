7da07dfea0a4d03a85c5656042465d4df92cdd39
Sarali Balasinghe <saralithiloka@gmail.com>
2024-11-30 12:05:50 +0530

    model class for user

---
+++ src/model/user.java
@@ -0,0 +1,33 @@
+package model;
+
+public class user {
+
+	String name;
+	String email;
+	String username;
+	String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
